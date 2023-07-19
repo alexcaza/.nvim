7e43968b32cce1cdb388bb54b86c4767358959b8
Alex Caza <alex@alexcaza.com>
2023-07-19 16:29:41 -0400

    Add buffer and window control

--- lua/alexcaza/remap.lua
+++ lua/alexcaza/remap.lua
@@ -6,10 +6,16 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Buffers
-vim.keymap.set("n", "<leader>ls", ":ls<Enter>")
+vim.keymap.set("n", "<leader>bl", ":ls<Enter>")
+vim.keymap.set("n", "<leader>bn", ":bnext<Enter>")
+vim.keymap.set("n", "<leader>bp", ":bprev<Enter>")
 
 -- Line numbers
 vim.keymap.set("n", "<leader>rn", ":set relativenumber!<Enter>")
 
 -- Lazygit
 vim.keymap.set("n", "<leader>gg", ":LazyGit<Enter>")
+
+-- Windows
+vim.keymap.set("n", "<leader>w", "<C-w>")
+
