a6901e9877bfc603b3ba235b9c1af352068e03c9
Alex Caza <alex@alexcaza.com>
2023-08-17 14:06:51 -0400

    Expand trouble config

--- lua/alexcaza/packer.lua
+++ lua/alexcaza/packer.lua
@@ -57,17 +57,7 @@ return require('packer').startup(function(use)
       }
   }
 
-  use {
-      "folke/trouble.nvim",
-      config = function()
-          require("trouble").setup {
-              icons = false,
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
-          }
-      end
-  }
+  use { "folke/trouble.nvim" }
 
   use {
       "kdheepak/lazygit.nvim",
