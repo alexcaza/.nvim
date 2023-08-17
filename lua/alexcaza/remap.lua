65e157c2ad22930457485cee14152e23487230c5
Alex Caza <alex@alexcaza.com>
2023-08-17 13:42:45 -0400

    Use telescope for buffer list

--- lua/alexcaza/remap.lua
+++ lua/alexcaza/remap.lua
@@ -6,7 +6,6 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Buffers
-vim.keymap.set("n", "<leader>bl", ":ls<Enter>")
 vim.keymap.set("n", "<leader>bn", ":bnext<Enter>")
 vim.keymap.set("n", "<leader>bp", ":bprev<Enter>")
 
