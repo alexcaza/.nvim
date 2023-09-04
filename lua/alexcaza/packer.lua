811e08f975edfc342c4a44d695da62aa9a36f829
Alex Caza <alex@alexcaza.com>
2023-09-03 21:42:08 -0400

    Add markdown previewer

--- lua/alexcaza/packer.lua
+++ lua/alexcaza/packer.lua
@@ -112,4 +112,17 @@ return require('packer').startup(function(use)
 
   use { 'f-person/git-blame.nvim' }
 
+  -- install without yarn or npm
+  use({
+      "iamcco/markdown-preview.nvim",
+      run = function() vim.fn["mkdp#util#install"]() end,
+  })
+
+  use({ 
+      "iamcco/markdown-preview.nvim", 
+      run = "cd app && npm install",
+      setup = function() vim.g.mkdp_filetypes = { "markdown" } end, 
+      ft = { "markdown" }, 
+  })
+
 end)
